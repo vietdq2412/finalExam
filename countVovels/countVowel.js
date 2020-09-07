178b0556f41f871b1dc581477f2af1491fec4e69
viet <vietdq2412@gmail.com>
2020-09-07 15:58:17 +0700

    fix counVowel

--- countVovels/countVowel.js
+++ countVovels/countVowel.js
@@ -3,10 +3,7 @@ const VOWELHIGH = ['U', 'E', 'O', 'A', 'I'];
 let str = prompt('input a string: ', ' ');
 let rs;
 
-while (str != ' ') {
-    alert('input a string!');
-    let str = prompt('input a string: ', '');
-}
+
 rs = countVovel(str);
 alert('có ' + rs + ' nguyên âm trong chuỗi');
 
